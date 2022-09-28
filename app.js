f453fe9a97486c4ca7494d8d0452d2fa4ebc9d54
Carlos Estrada <cestrada1109@gmail.com>
2022-09-27 23:36:34 -0500

    Add env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 const compression = require('compression')
 const helmet = require('helmet')
 
@@ -15,7 +16,7 @@ var app = express();
 
 // Set up mongoose connection 
 const mongoose = require("mongoose")
-const mongoDB = "mongodb+srv://carloze11:locallibrarymdn@cluster0.7jgo4qp.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
